d2f586c8af0e4c9aca61fe0608223e97570d5142
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2023-04-16 07:53:43 +0800

    add celcius to fahrenheit

--- main.cpp
+++ main.cpp
@@ -6,6 +6,39 @@ using namespace std;
 
 int main()
 {
-    cout << "t" << "" << "Temperature" << "\n" << endl;
+    char unit;
+    char convert;
+    double temp;
+    char to;
+
+    cout << "T" << " " << "Temperature" << "\n" << endl;
+    
+    cout << "Select unit:" << " " << endl;
+    cin >> unit;
+
+    cout << "C = Celcius" << endl;
+    cout << "F = Fahrenheit" << endl;
+    cout << "K = Kelvin" << endl;
+    cin >> convert;
+
+    if (unit == 'T' || unit == 't')
+    {
+        if (convert == 'C' || convert == 'c')
+        {
+            cout << "Enter temperature to convert:" << " " << endl;
+            cin >> temp;
+            cout << "to:" << " " << endl;
+            cin >> to;
+            
+            if (temp == 'T' || 't')
+            {
+                double result = (5/9)*(temp - 32);
+            }
+        }
+    }
+    else
+    {
+        cout << "Invalid input" << endl;
+    }
 
 }
