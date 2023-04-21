b77f27e46148493e3c80fc107f07ce8b33281a2c
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2023-04-21 14:31:26 +0800

    update temperature function

--- src/main.cpp
+++ src/main.cpp
@@ -59,24 +59,21 @@ int main()
 
             if (convert_upper == 'C')
             {
-                cout << "Enter temperature:" << " ";
-                cin >> _temp_;
+                input_temperature();
 
                 cout << "Fahrenheit:" << " " << ((temp * 9)/5) + 32 << endl;
                 cout << "Kelvin:" << " " << temp + 273.15 << endl;
             }
             else if (convert_upper == 'F')
             {
-                cout << "Enter temperature:" << " ";
-                cin >> _temp_;
+                input_temperature();
 
             	cout << "Celcius:" << " " << ((temp - 32)*5)/9 << endl;
                 cout << "Kelvin:" << " " << temp + 459.67 << endl;
             }
             else if (convert_upper == 'K')
             {
-                cout << "Enter temperature:" << " ";
-                cin >> _temp_;
+                input_temperature();
 
                 cout << "Celcius:" << " " << (temp - 273.15) << endl;
                 cout << "Fahrenheit:" << " " << ((temp - 273.15)*9)/5 + 32 << endl;
