86e44cd9161ad6e011f24f950117236b5bb5672d
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2023-04-21 14:08:35 +0800

    use string vector

--- src/main.cpp
+++ src/main.cpp
@@ -6,6 +6,12 @@ using namespace std;
 
 int main()
 {
+    vector<string> temperature =
+    {
+    "(C) Celcius",
+    "(F) Fahrenheit",
+    "(K) Kelvin"
+    };
 
     char unit;
     char convert;
