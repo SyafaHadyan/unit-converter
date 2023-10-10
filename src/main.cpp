c99a33d02b9387a00825cfdd8d0f220daaaeef1a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2023-10-10 09:42:05 +0800

    Refactor

--- src/main.cpp
+++ src/main.cpp
@@ -18,25 +18,25 @@ char convert_upper_toupper(string c)
 
 void enter_unit_convert()
 {
-    cout << "Enter Unit:" << " ";
+    cout << "Enter Unit" << "" << ":" << " ";
     cin >> convert;
 }
 
 void input_temperature()
 {
-    cout << "Enter temperature:" << " ";
+    cout << "Enter temperature" << "" << ":" << " ";
     cin >> temp;
 }
 
 void input_weight()
 {
-    cout << "Enter weight:" << " ";
+    cout << "Enter weight" << ":" << " ";
     cin >> weight;
 }
 
 void input_length()
 {
-    cout << "Enter length:" << " ";
+    cout << "Enter length" << ":" << " ";
     cin >> length;
 }
 
@@ -75,7 +75,7 @@ int main()
         cout << "(W)" << " " << "Weight" << "\n" << endl;
         cout << "(L)" << " " << "Length" << "\n" << endl;
         
-        cout << "Select unit:" << " ";
+        cout << "Select unit" << ":" << " ";
         cin >> unit_select;
 
         if (unit_select == 'T' || unit_select == 'W' || unit_select == 'L')
@@ -94,22 +94,22 @@ int main()
                 {
                     input_temperature();
 
-                    cout << "Fahrenheit:" << " " << ((temp * 9) / 5) + 32 << endl;
-                    cout << "Kelvin:" << " " << temp + 273.15 << endl;
+                    cout << "Fahrenheit" << ":" << " " << ((temp * 9) / 5) + 32 << endl;
+                    cout << "Kelvin" << ":" << " " << temp + 273.15 << endl;
                 }
                 else if (convert == "F")
                 {
                     input_temperature();
 
-                    cout << "Celcius:" << " " << ((temp - 32) * 5) / 9 << endl;
-                    cout << "Kelvin:" << " " << temp + 459.67 << endl;
+                    cout << "Celcius" << ":" << " " << ((temp - 32) * 5) / 9 << endl;
+                    cout << "Kelvin" << ":" << " " << temp + 459.67 << endl;
                 }
                 else if (convert == "K")
                 {
                     input_temperature();
 
-                    cout << "Celcius:" << " " << (temp - 273.15) << endl;
-                    cout << "Fahrenheit:" << " " << ((temp - 273.15) * 9) / 5 + 32 << endl;
+                    cout << "Celcius" << ":" << " " << (temp - 273.15) << endl;
+                    cout << "Fahrenheit" << ":" << " " << ((temp - 273.15) * 9) / 5 + 32 << endl;
                 }
             }
             else if (unit_select == 'W')
@@ -126,45 +126,45 @@ int main()
                 {
                     input_weight();
 
-                    cout << "Kilogram:" << " " << weight * 1000 << endl;
-                    cout << "Milligram:" << " " << weight * 1000000 << endl;
-                    cout << "Ounce:" << " " << weight * 0.03527396195 << endl;
-                    cout << "Pound:" << " " << (weight / 1000) * 2.2046 << endl;
-                    cout << "Ton:" << " " << (weight / 1000) * 0.00110231 << endl;
-                    cout << "Stone:" << " " << (weight / 1000) * 0.157473 << endl;
+                    cout << "Kilogram" << ":" << " " << weight * 1000 << endl;
+                    cout << "Milligram" << ":" << " " << weight * 1000000 << endl;
+                    cout << "Ounce" << ":" << " " << weight * 0.03527396195 << endl;
+                    cout << "Pound" << ":" << " " << (weight / 1000) * 2.2046 << endl;
+                    cout << "Ton" << ":" << " " << (weight / 1000) * 0.00110231 << endl;
+                    cout << "Stone" << ":" << " " << (weight / 1000) * 0.157473 << endl;
                 }
                 else if (convert == "KG")
                 {
                     input_weight();
 
-                    cout << "Gram:" << " " << weight / 1000 << endl;
-                    cout << "Milligram:" << " " << weight / 1000000 << endl;
-                    cout << "Ounce:" << " " << weight / 0.03527396195 << endl;
-                    cout << "Pound:" << " " << (weight / 1000) * 2.2046 << endl;
-                    cout << "Ton:" << " " << (weight / 1000) * 0.00110231 << endl;
-                    cout << "Stone:" << " " << (weight / 1000) * 0.157473 << endl;
+                    cout << "Gram" << ":" << " " << weight / 1000 << endl;
+                    cout << "Milligram" << ":" << " " << weight / 1000000 << endl;
+                    cout << "Ounce" << ":" << " " << weight / 0.03527396195 << endl;
+                    cout << "Pound" << ":" << " " << (weight / 1000) * 2.2046 << endl;
+                    cout << "Ton" << ":" << " " << (weight / 1000) * 0.00110231 << endl;
+                    cout << "Stone" << ":" << " " << (weight / 1000) * 0.157473 << endl;
                 }
                 else if (convert == "TON")
                 {
                     input_weight();
 
-                    cout << "Gram:" << " " << weight / 0.000001 << endl;
-                    cout << "Kilogram:" << " " << weight / 0.00110231 << endl;
-                    cout << "Milligram:" << " " << weight / 0.000000001 << endl;
-                    cout << "Ounce:" << " " << weight / 0.00003527396195 << endl;
-                    cout << "Pound:" << " " << weight / 0.0022046 << endl;
-                    cout << "Stone:" << " " << weight / 0.157473 << endl;
+                    cout << "Gram" << ":" << " " << weight / 0.000001 << endl;
+                    cout << "Kilogram" << ":" << " " << weight / 0.00110231 << endl;
+                    cout << "Milligram" << ":" << " " << weight / 0.000000001 << endl;
+                    cout << "Ounce" << ":" << " " << weight / 0.00003527396195 << endl;
+                    cout << "Pound" << ":" << " " << weight / 0.0022046 << endl;
+                    cout << "Stone" << ":" << " " << weight / 0.157473 << endl;
                 }
                 else if (convert == "ST")
                 {
                     input_weight();
 
-                    cout << "Gram:" << " " << weight / 0.000001 << endl;
-                    cout << "Kilogram:" << " " << weight / 0.00110231 << endl;
-                    cout << "Milligram:" << " " << weight / 0.000000001 << endl;
-                    cout << "Ounce:" << " " << weight / 0.00003527396195 << endl;
-                    cout << "Pound:" << " " << weight / 0.0022046 << endl;
-                    cout << "Ton:" << " " << weight / 0.000157473 << endl;
+                    cout << "Gram" << ":" << " " << weight / 0.000001 << endl;
+                    cout << "Kilogram" << ":" << " " << weight / 0.00110231 << endl;
+                    cout << "Milligram" << ":" << " " << weight / 0.000000001 << endl;
+                    cout << "Ounce" << ":" << " " << weight / 0.00003527396195 << endl;
+                    cout << "Pound" << ":" << " " << weight / 0.0022046 << endl;
+                    cout << "Ton" << ":" << " " << weight / 0.000157473 << endl;
                 }
             }
             else if (unit_select == 'L') {
@@ -180,25 +180,31 @@ int main()
                 {
                     input_length();
 
-                    cout << "CM:" << " " << length / 10 << endl;
-                    cout << "M:" << " " << length / 1000 << endl;
-                    cout << "KM:" << " " << length / 1000000 << endl;
-                    cout << "IN:" << " " << length / 25.4 << endl;
-                    cout << "FT:" << " " << length / 25.4 << endl;
+                    cout << "CM" << ":" << " " << length / 10 << endl;
+                    cout << "M" << ":" << " " << length / 1000 << endl;
+                    cout << "KM" << ":" << " " << length / 1000000 << endl;
+                    cout << "IN" << ":" << " " << length / 25.4 << endl;
+                    cout << "FT" << ":" << " " << length / 25.4 << endl;
                 }
                 else if (convert == "CM")
                 {
                     input_length();
 
-                    cout << "MM:" << " " << length / 10 << endl;
-                    cout << "M:" << " " << length / 10 << endl;
-                    cout << "KM:" << " " << length / 100000 << endl;
-                    cout << "IN:" << " " << length / 2.54 << endl;
-                    cout << "FT:" << " " << length / 30.48 << endl;
+                    cout << "MM" << ":" << " " << length / 10 << endl;
+                    cout << "M" << ":" << " " << length / 10 << endl;
+                    cout << "KM" << ":" << " " << length / 100000 << endl;
+                    cout << "IN" << ":" << " " << length / 2.54 << endl;
+                    cout << "FT" << ":" << " " << length / 30.48 << endl;
                 }
                 else if (convert == "M")
                 {
                     input_length();
+
+                    cout << "MM" << ":" << " " << length / 1000 << endl;
+                    cout << "CM" << ":" << " " << length / 100 << endl;
+                    cout << "KM" << ":" << " " << length / 1000000 << endl;
+                    cout << "IN" << ":" << " " << length / 1000 << endl;
+                    cout << "FT" << ":" << " " << length / 30.48 << endl;
                 }
                 else if (convert == "KM")
                 {
