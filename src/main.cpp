a14433926261305ef251ebb78b00c836d58bf31a
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2023-10-29 08:15:58 +0800

    Feature: Add GIB conversion

--- src/main.cpp
+++ src/main.cpp
@@ -1587,6 +1587,30 @@ int main()
                     cout << "Zebibyte" << ":" << " " << data_value * 0.00000000000000088817842 << endl;
                     cout << "Yobibyte" << ":" << " " << data_value * 0.00000000000000000086736174 << endl;
                 }
+                else if (convert == "GIB")
+                {
+                    input_data();
+
+                    newline();
+
+                    cout << "Byte" << ":" << " " << data_value * 1073741824 << endl;
+                    cout << "Kilobyte" << ":" << " " << data_value * 1048576 << endl;
+                    cout << "Megabyte" << ":" << " " << data_value * 1024 << endl;
+                    cout << "Gigabyte" << ":" << " " << data_value * 0.0009765625 << endl;
+                    cout << "Terabyte" << ":" << " " << data_value * 0.000000953674316 << endl;
+                    cout << "Petabyte" << ":" << " " << data_value * 0.000000000931322575 << endl;
+                    cout << "Exabyte" << ":" << " " << data_value * 0.000000000000909494702 << endl;
+                    cout << "Zettabyte" << ":" << " " << data_value * 0.00000000000000088817842 << endl;
+                    cout << "Yottabyte" << ":" << " " << data_value * 0.00000000000000000086736174 << endl;
+                    cout << "Bit" << ":" << " " << data_value * 8589934592 << endl;
+                    cout << "Kibibyte" << ":" << " " << data_value * 1048576 << endl;
+                    cout << "Mebibyte" << ":" << " " << data_value * 1024 << endl;
+                    cout << "Tebibyte" << ":" << " " << data_value * 0.0009765625 << endl;
+                    cout << "Pebibyte" << ":" << " " << data_value * 0.000000953674316 << endl;
+                    cout << "Exbibyte" << ":" << " " << data_value * 0.000000000931322575 << endl;
+                    cout << "Zebibyte" << ":" << " " << data_value * 0.000000000000909494702 << endl;
+                    cout << "Yobibyte" << ":" << " " << data_value * 0.00000000000000088817842 << endl;
+                }
             }
         }
         else
