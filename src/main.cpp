b8ca0c7f304ecd85dbd99700581a7a577fc3fc44
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-08-30 07:01:45 +0700

    Add: SQKM to ACRE

--- src/main.cpp
+++ src/main.cpp
@@ -1803,6 +1803,7 @@ int main()
                     cout << "Square Inch" << ":" << " " << area_value * 1550003100.0062 << endl;
                     cout << "Square Nautical Mile" << ":" << " " << area_value * 0.291553350 << endl;
                     cout << "Hectare" << ":" << " " << area_value * 100 << endl;
+                    cout << "Acre" << ":" << " " << area_value * 247.10538147 << endl;
                 }
             }
         }
