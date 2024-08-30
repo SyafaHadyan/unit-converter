e38e065589db105c0333b46b40c1834f9b550986
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-08-30 07:09:17 +0700

    Add: SQCM to SQMM

--- src/main.cpp
+++ src/main.cpp
@@ -1813,6 +1813,7 @@ int main()
 
                     cout << "Square Meter" << ":" << " " << area_value * 0.0001 << endl;
                     cout << "Square Kilometer" << ":" << " " << area_value * 0.0000000001 << endl;
+                    cout << "Square Milimeter" << ":" << " " << area_value * 100 << endl;
                 }
             }
         }
