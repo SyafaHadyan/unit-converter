c18274fd2e265531b3b196a1e4ad1c5af69531f4
Syafa Hadyan Rasendriya <syafahadyanrasendriya@gmail.com>
2024-09-03 07:02:19 +0700

    Add: SQMM to SQFT

--- src/main.cpp
+++ src/main.cpp
@@ -1837,6 +1837,7 @@ int main()
                     cout << "Square Nanometer" << ":" << " " << area_value * 1000000 << endl;
                     cout << "Square Mile" << ":" << " " << area_value * 0.000000003861018768 << endl;
                     cout << "Square Yard" << ":" << " " << area_value * 0.0000011959900463011 << endl;
+                    cout << "Square Foot" << ":" << " " << area_value * 0.0032808399 << endl;
                 }
             }
         }
